0859e08a1210a39d38ebe90ba606364cc7983059
Dariusz Sibik <sibikdariusz@gmail.com>
2019-02-18 21:46:54 +0100

    Implementation of Global Event Bus

---
+++ src/app/shared/model/test-lessons.ts
@@ -0,0 +1,19 @@
+import {Lesson} from "./lesson";
+
+export const lessons: Lesson[] = [
+  {
+    id: 1,
+    description: 'Setting up and Angular Development',
+    duration: '5:00'
+  },
+  {
+    id: 2,
+    description: 'Running the the lessons Code',
+    duration: '7:00'
+  },
+  {
+    id: 3,
+    description: 'Build Your First App - Hello World Step By Step',
+    duration: '9:00'
+  }
+]
