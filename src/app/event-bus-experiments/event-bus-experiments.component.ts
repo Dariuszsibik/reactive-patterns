043cceb9935dd88849eda61068e3f2afd1c4f0d1
Dariusz Sibik <sibikdariusz@gmail.com>
2019-02-20 22:44:40 +0100

    Improve Global Event Bus

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { globalEventBus } from './event-bus';
+import { globalEventBus, LESSONS_LIST_AVAILABLE } from './event-bus';
 import { testLessons } from '../shared/model/test-lessons';
 
 @Component({
@@ -11,7 +11,8 @@ export class EventBusExperimentsComponent implements OnInit {
 
   ngOnInit() {
 
-    globalEventBus.notifyObserver(testLessons);
+    globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE,
+        testLessons.slice(0));
 
   }
 
