cd8b75148589f2defe8ba225bb8a780b86f8d33b
Dariusz Sibik <sibikdariusz@gmail.com>
2019-02-20 23:08:41 +0100

    addLesson()

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { globalEventBus, LESSONS_LIST_AVAILABLE } from './event-bus';
+import { globalEventBus, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from './event-bus';
 import { testLessons } from '../shared/model/test-lessons';
 
 @Component({
@@ -14,6 +14,10 @@ export class EventBusExperimentsComponent implements OnInit {
     globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE,
         testLessons.slice(0));
 
+  }
+
+  addLesson(lessonText: string) {
+    globalEventBus.notifyObservers(ADD_NEW_LESSON, lessonText)
   }
 
 }
